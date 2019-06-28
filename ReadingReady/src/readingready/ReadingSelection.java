7a6fe9b4b7dfe915592daf8902138eb851155723
Hannah Saliot <hannah_saliot@dlsu.edu.ph>
2019-06-28 20:14:00 +0800

    db entities started

---
+++ ReadingReady/src/readingready/ReadingSelection.java
@@ -0,0 +1,68 @@
+package readingready;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Hannah Saliot
+ */
+@Entity
+@Table(name = "READINGSELECTION")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "ReadingSelection.findAll", query = "SELECT r FROM ReadingSelection r"),
+    @NamedQuery(name = "ReadingSelection.findById", query = "SELECT r FROM ReadingSelection r WHERE r.id = :id"),
+    @NamedQuery(name = "ReadingSelection.findByTitle", query = "SELECT r FROM ReadingSelection r WHERE r.title = :title")})
+public class ReadingSelection implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    private Integer id;    
+    private String title;
+    private String text;
+
+    public ReadingSelection(Integer id) {
+        this.id = id;
+    }
+
+    public ReadingSelection(Integer id, String title, String text) {
+        this.id = id;
+        this.title = title;
+        this.text = text;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
